49ce71697b9fedbd88f6d1d2a6fccbf2c30b3dd6
rachwo <47090281+rachwo@users.noreply.github.com>
2022-07-08 12:00:24 -0700

    Update and rename cytoTrans.R to 2-cytoTrans.R

--- 2-cytoTrans.R
+++ 2-cytoTrans.R
@@ -1,17 +1,17 @@
 # read in flow files and perform normalization and arcsinh transformation
 
-cytoTrans <- function(ref_flow_foldername,
-                      flow_foldername_1 = NULL,
-                      flow_foldername_2 = NULL,
-                      normalize = NULL,
-                      plot_norm = NULL) {
+cytoTrans <- function(normalize = NULL,
+                      plot_norm = NULL,
+                      exclude = NULL) {
+  
   
   # create list of flowjo scale files from target directory (exclude bead files)
   file_list <- list.files(path = ".", pattern = "*.csv", recursive = T, full.names = FALSE) %>% 
-    stringr::str_subset(., "Beads", negate = T)
+    stringr::str_subset(., "Beads.csv", negate = T)
+  
   
   # add column indicating original file name
-  csv_filename <- function(filename){
+  csv_filename <- function(filename) {
     ret <- read.csv(here::here(filename))
     sample <- sub('.csv*', '', filename)
     ret$Well <- sub('.*/', '', filename)
@@ -21,55 +21,61 @@ cytoTrans <- function(ref_flow_foldername,
   
   mat.full <- plyr::ldply(file_list, csv_filename)
   
-  # remove unwanted channels
-  mat <- mat.full %>% select(-contains(c("FSC","SSC","Time","GFP","DAPI","Live")))
+  
+  # remove unwanted channels (include channels already removed at the normalization step, if applicable)
+  if(!is.null(removed) == TRUE) {
+    removed <- removed
+  }
+  
+  mat <- mat.full %>% select(-contains(c("FSC","SSC","Time","GFP","DAPI","Live","Viability", exclude, removed)))
+  
   
   # define transformation
   asinhtransform <- flowCore::arcsinhTransform(a = 0, b = 1/150)
   
+  
   # perform per-channel normalization
   if (isTRUE(normalize)) {
     
-    # double check that cytoNorm was run successfully.
+    # double check that cytoNorm was run successfully
     if (exists("norm.fact") != TRUE) {
-      
       print("Error: Can't perform normalization unless cytoNorm has been run. Please run cytoNorm first.")
     }
     
     # get number of columns for each dataframe 
-    len <- length(norm.fact) - 2
+    len <- length(norm.fact) - 1
     norm.len <- len + length(norm.fact)
     
     # join normalization vector with matrix 
-    mat <- inner_join(mat, norm.fact[,c(1:len,length(norm.fact))], by = "Folder")
+    mat <- inner_join(mat, norm.fact[, c(1:len,length(norm.fact))], by = "Folder", suffix = c("",".norm"))
     
     # multiply unnormalized data by normalization factor
-    mat.norm <- mat[1:len]*mat[(length(norm.fact)+1):(norm.len)]
+    mat.norm <- mat[1:len] * mat[(length(norm.fact) + 2):(norm.len + 1)]
     mat.norm$Well <- mat$Well
     mat.norm$Folder <- mat$Folder
     
     # generate histograms of normalized and unnormalized data
     if (isTRUE(plot_norm)) {
       
-      mat.plot <- mat[,c(1:length(norm.fact))] %>% 
+      mat.plot <- mat[, c(1:length(norm.fact))] %>% 
         mutate(dataset = "unnormalized") %>% 
         full_join(mat.norm %>% mutate(dataset = "normalized")) %>% 
-        mutate(plot = paste0(dataset,"_",Well))
+        mutate(plot = paste0(dataset, "_", Well))
       
       colNames <- names(mat.norm[1:len])
       
       for (i in colNames) {
-        
+  
         p <- ggplot(mat.plot, aes_string(x = i, y = "plot", fill = "Well", color = "Well")) +
           geom_density_ridges2(alpha = 0.5, 
                                show.legend = FALSE,
-                               quantile_lines=TRUE,
-                               quantiles=2) + 
+                               quantile_lines = TRUE,
+                               quantiles = 2) + 
           theme_minimal() + 
-          theme(panel.grid=element_blank(),
+          theme(panel.grid = element_blank(),
                 axis.title.y = element_blank(),
-                axis.text=element_text(size=11),
-                axis.text.x=element_blank())
+                axis.text = element_text(size = 11),
+                axis.text.x = element_blank())
         print(p)
       }
     }
@@ -82,7 +88,7 @@ cytoTrans <- function(ref_flow_foldername,
       asinhtransform() %>% 
       mutate(Well = mat$Well) %>% 
       mutate(Folder = mat$Folder) %>% 
-      mutate(plot = paste0(Folder,"_",Well))
+      mutate(plot = paste0(Folder, "_", Well))
     
     # export dataframe to envir 
     assign(x = "matrix", value = mat.ast, envir = parent.frame())
@@ -91,9 +97,7 @@ cytoTrans <- function(ref_flow_foldername,
   
   # prevent user from plotting normalized results if normalization is not specified
   else if (isTRUE(plot_norm) && (isFALSE(normalize) | is.null(normalize))) {
-    
     print("Error: can't plot normalization results if normalize is not set to TRUE. Do you want to normalize the data?")
-    
   }
   
   else {
@@ -104,7 +108,7 @@ cytoTrans <- function(ref_flow_foldername,
       asinhtransform() %>% 
       mutate(Well = mat$Well) %>% 
       mutate(Folder = mat$Folder) %>% 
-      mutate(plot = paste0(Folder,"_",Well))
+      mutate(plot = paste0(Folder, "_", Well))
     
     # export dataframe to envir 
     assign(x = "matrix", value = mat.ast, envir = parent.frame()) 
