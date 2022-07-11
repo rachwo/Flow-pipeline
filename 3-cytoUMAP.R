03a42253df515dbcc3448d953c6f568adf041392
rachwo <47090281+rachwo@users.noreply.github.com>
2022-07-11 13:30:20 -0700

    Update 3-cytoUMAP.R

--- 3-cytoUMAP.R
+++ 3-cytoUMAP.R
@@ -11,23 +11,17 @@ cytoUMAP <- function(min_nn,
                      min_dist, 
                      downsample = NULL){
   
-  
   # generate error messages
   if(min_nn < 2){
     print("ERROR: Minimum number of neighbours must be >=2")
-  }
-  
-  if (min_dist <= 0) { 
+    
+  } else if (min_dist <= 0) { 
     print("ERROR: Minimum distance must be >0")
-  }
-  
-  if(max_nn < interval){
+    
+  } else if(max_nn < interval){
     print("ERROR: Interval must be smaller than maximum number of neighbours")
-  }
-  
-  else if(min_nn >= 2 && max_nn > interval && min_dist > 0) {
     
-    
+  } else {
     # get range of neighbours to test
     range <- seq(from = min_nn, to = max_nn, by = interval)
     
@@ -36,22 +30,23 @@ cytoUMAP <- function(min_nn,
     
     # downsample matrix (optional)
     if (!is.null(downsample)) {
-      mat.down <- with(matrix, ave(matrix[, 1], plot, FUN = function(x) {sample.int(length(x), replace = FALSE)}))
+      mat.down <- with(matrix, 
+                       ave(matrix[, 1], 
+                       plot, 
+                       FUN = function(x) {sample.int(length(x), replace = FALSE)}))
       matrix.d <- matrix[mat.down <= as.numeric(downsample), ]
       
       assign(x = "matrix.down", value = matrix.d, envir = parent.frame())
-    }
-    
-    else if (is.null(downsample)){
-      matrix.d <- matrix
-    }
+      
+    } else if (is.null(downsample)) { matrix.d <- matrix }
     
     # parallellize
     myCluster <- makeCluster(10, type = "FORK") 
     registerDoParallel(myCluster)
     
     # run umap
-    umap.out <- foreach(x = range) %dopar% uwot::umap(matrix.d, n_neighbors = x, 
+    umap.out <- foreach(x = range) %dopar% uwot::umap(matrix.d, 
+                                                      n_neighbors = x, 
                                                       min_dist = min_dist, 
                                                       ret_model = TRUE, 
                                                       verbose = T)
@@ -60,7 +55,8 @@ cytoUMAP <- function(min_nn,
     
     # generate plots 
     for (i in 1:length(umap.out)) {
-      p = ggplot(data = as.data.frame(umap.out[[i]]$embedding), aes(x = V1, y = V2, col = matrix.down$Folder)) +
+      p = ggplot(data = as.data.frame(umap.out[[i]]$embedding), 
+                 aes(x = V1, y = V2, col = matrix.down$Folder)) +
         geom_point(size = 0.5, alpha = 0.5) +
         scale_x_continuous(expand = c(0.1, 0)) + 
         scale_y_continuous(expand = c(0.1, 0)) +
@@ -79,10 +75,7 @@ cytoUMAP <- function(min_nn,
       ggsave(p, path = "./Neighbour_plots",
              filename = paste("UMAP", umap.out[[i]][["n_neighbors"]], "neighbours.png"), 
              width=14, height=10, units="cm")
-      
     }
-    
     on.exit(stopCluster(myCluster))
-    
   }
 }
